b0318c08866357cfaf356f2d8b9f59e3438a5d77
Blenda Orellana <blen.or90@gmail.com>
2021-07-25 14:20:06 -0500

    got index in models to work

--- models/index.js
+++ models/index.js
@@ -1,29 +1,29 @@
-// const User = require('./User');
-// const Post = require('./Post');
-// const Comment = require('./Comment');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-// User.hasMany(Post, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Post, {
+  foreignKey: 'User_id',
+  onDelete: 'CASCADE'
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Comment, {
+  foreignKey: 'User_id',
+  onDelete: 'CASCADE'
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Post.belongsTo(User, {
+  foreignKey: 'User_id'
+});
 
-// Post.hasMany(Comment, {
-//   foreignKey: 'post_id',
-//   onDelete: 'CASCADE'
-// });
+Post.hasMany(Comment, {
+  foreignKey: 'Post_id',
+  onDelete: 'CASCADE'
+});
 
-// Comment.belongsTo(Post, {
-//     foreignKey: 'post_id'
-// });
+Comment.belongsTo(Post, {
+    foreignKey: 'Post_id'
+});
 
-// module.exports = { User, Post, Comment };
+module.exports = { User, Post, Comment };
 
